9f4969ec78e461c0ab9679b46b46c4abc16324e1
wimb0 <w.baars@wimbo.nl>
2021-03-05 10:49:56 +0100

    status fix

--- custom_components/saj_modbus/sensor.py
+++ custom_components/saj_modbus/sensor.py
@@ -100,7 +100,6 @@ class SajSensor(Entity):
         if self._key in ["mpvmode", "statusvendor"]:
             _LOGGER.debug("Key %s = %s", self._key, self.state)
             if self.state in DEVICE_STATUSSES:
-                _LOGGER.debug("Status %s = %s", ATTR_STATUS_DESCRIPTION, DEVICE_STATUSSES[])
                 return {ATTR_STATUS_DESCRIPTION: DEVICE_STATUSSES[]}
         return None
 
