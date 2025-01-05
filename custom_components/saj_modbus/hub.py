48e4cb1cce9cdbac254960a9ba5bc6fc2f11c58c
Wim <11030068+wimb0@users.noreply.github.com>
2025-01-05 12:19:07 +0100

    Update hub.py

--- custom_components/saj_modbus/hub.py
+++ custom_components/saj_modbus/hub.py
@@ -1,6 +1,6 @@
 """SAJ Modbus Hub"""
 from pymodbus.pdu.register_read_message import ReadHoldingRegistersResponse
-from pymodbus.pdu.register_write_message import ModbusResponse
+from pymodbus.register_write_message import ModbusResponse
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 from voluptuous.validators import Number
 import logging
