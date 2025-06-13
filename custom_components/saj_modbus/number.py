1c4286c46da4e70b73ad70dd9763d16fe80e1337
Wim <11030068+wimb0@users.noreply.github.com>
2025-06-13 16:04:19 +0200

    Update number.py

--- custom_components/saj_modbus/number.py
+++ custom_components/saj_modbus/number.py
@@ -6,7 +6,10 @@ import logging
 
 from homeassistant.components.number import NumberEntity
 from homeassistant.const import CONF_NAME
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from homeassistant.config_entries import ConfigEntry
 
 from .const import (
     ATTR_MANUFACTURER,
@@ -19,16 +22,16 @@ from .hub import SAJModbusHub
 _LOGGER = logging.getLogger(__name__)
 
 
-async def async_setup_entry(hass, entry, async_add_entities):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
     """Set up entry for hub."""
     hub_name = entry.data[CONF_NAME]
     hub = hass.data[DOMAIN][hub_name]["hub"]
 
-    device_info = {
-        "identifiers": {(DOMAIN, hub_name)},
-        "name": hub_name,
-        "manufacturer": ATTR_MANUFACTURER,
-    }
+    device_info = DeviceInfo(
+        identifiers={(DOMAIN, hub_name)},
+        name=hub_name,
+        manufacturer=ATTR_MANUFACTURER,
+    )
 
     entities = []
     for number_description in NUMBER_TYPES.values():
@@ -47,21 +50,21 @@ async def async_setup_entry(hass, entry, async_add_entities):
 class SajNumber(CoordinatorEntity, NumberEntity):
     """Representation of an SAJ Modbus number."""
 
+    _attr_has_entity_name = True
     coordinator: SAJModbusHub
+    entity_description: SajModbusNumberEntityDescription
 
     def __init__(
         self,
         platform_name: str,
         hub: SAJModbusHub,
-        device_info,
+        device_info: DeviceInfo,
         description: SajModbusNumberEntityDescription,
     ):
         """Initialize the sensor."""
-        self._platform_name = platform_name
         self._attr_device_info = device_info
         self._attr_unique_id = f"{platform_name}_{description.key}"
-        self.entity_description: SajModbusNumberEntityDescription = description
-
+        self.entity_description = description
         super().__init__(coordinator=hub)
 
     @property
@@ -74,6 +77,8 @@ class SajNumber(CoordinatorEntity, NumberEntity):
         """Return the state of the number entity."""
         return self.coordinator.data.get(self.entity_description.key, None)
 
-    def set_native_value(self, value: float) -> None:
+    async def async_set_native_value(self, value: float) -> None:
         """Update the current value."""
-        self.coordinator.set_value(self.entity_description.key, value)
+        await self.coordinator.hass.async_add_executor_job(
+            self.coordinator.set_value, self.entity_description.key, value
+        )
