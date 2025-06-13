b2f463be6a9b5b33e292cac103c5f3f606a07cb0
Wim <11030068+wimb0@users.noreply.github.com>
2025-06-13 16:02:58 +0200

    Update __init__.py

--- custom_components/saj_modbus/__init__.py
+++ custom_components/saj_modbus/__init__.py
@@ -3,15 +3,12 @@
 import asyncio
 import logging
 
-import homeassistant.helpers.config_validation as cv
-import voluptuous as vol
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_HOST, CONF_NAME, CONF_PORT, CONF_SCAN_INTERVAL
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
 
 from .const import (
-    DEFAULT_NAME,
     DEFAULT_SCAN_INTERVAL,
     DOMAIN,
 )
@@ -20,28 +17,8 @@ from .services import async_setup_services
 
 _LOGGER = logging.getLogger(__name__)
 
-SAJ_MODBUS_SCHEMA = vol.Schema(
-    {
-        vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-        vol.Required(CONF_HOST): cv.string,
-        vol.Required(CONF_PORT): cv.string,
-        vol.Optional(
-            CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL
-        ): cv.positive_int,
-    }
-)
-
-CONFIG_SCHEMA = vol.Schema(
-    {DOMAIN: vol.Schema({cv.slug: SAJ_MODBUS_SCHEMA})}, extra=vol.ALLOW_EXTRA
-)
-
 PLATFORMS = ["sensor", "number"]
 
-
-async def async_setup(hass, config):
-    """Set up the SAJ modbus component."""
-    hass.data[DOMAIN] = {}
-    return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up a SAJ mobus."""
@@ -73,13 +50,11 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload SAJ modbus entry."""
 
-    unload_ok = all(
-        await asyncio.gather(
-            *[
-                hass.config_entries.async_forward_entry_unload(entry, component)
-                for component in PLATFORMS
-            ]
-        )
+    unload_ok = await asyncio.gather(
+        *[
+            hass.config_entries.async_forward_entry_unload(entry, component)
+            for component in PLATFORMS
+        ]
     )
     if not unload_ok:
         return False
