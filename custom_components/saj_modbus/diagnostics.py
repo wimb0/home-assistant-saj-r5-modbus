575e2219c23b3a8e1cf31ba8d0a3a926ce5b4ec3
Wim <11030068+wimb0@users.noreply.github.com>
2025-08-20 22:35:28 +0200

    Update diagnostics.py

--- custom_components/saj_modbus/diagnostics.py
+++ custom_components/saj_modbus/diagnostics.py
@@ -3,23 +3,32 @@ from __future__ import annotations
 
 from typing import Any
 
+from homeassistant.components.diagnostics import async_redact_data
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_NAME
+from homeassistant.const import CONF_HOST, CONF_NAME, CONF_PORT
 from homeassistant.core import HomeAssistant
 
-from .const import DOMAIN
 from .hub import SAJModbusHub
+
+# Data to redact from diagnostics
+TO_REDACT = {
+    CONF_HOST,
+    CONF_NAME,
+    CONF_PORT,
+}
+
 
 async def async_get_config_entry_diagnostics(
     hass: HomeAssistant, entry: ConfigEntry
 ) -> dict[str, Any]:
     """Return diagnostics for a config entry."""
-    hub: SAJModbusHub = hass.data[DOMAIN][entry.data[CONF_NAME]]["hub"]
+    hub: SAJModbusHub = entry.runtime_data
 
+    # Gather all relevant data for debugging
     diagnostics_data = {
-        "config_entry_data": entry.data,
-        "config_entry_options": entry.options,
-        "inverter_data": hub.inverter_data,
+        "config_entry_data": async_redact_data(entry.data, TO_REDACT),
+        "config_entry_options": async_redact_data(entry.options, TO_REDACT),
+        "inverter_data": async_redact_data(hub.inverter_data, TO_REDACT),
         "last_fetched_data": hub.data,
     }
 
